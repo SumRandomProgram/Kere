6f9b4b5dee2b0da40ce26009eacab7f696e966e3
SumRandomProgram <edvards.vaitelis@gmail.com>
2025-04-28 15:24:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// This script runs when the website loads
+console.log("Website Loaded Successfully!");
+
+document.addEventListener('DOMContentLoaded', () => {
+  alert('Welcome to my first website!');
+});
